7905fe5fdcf49bddedf3edc52cbf4929b950afe4
zubayrrr <zubayrali@protonmail.com>
2021-01-04 11:09:08 +0530

    first commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./homepage.styles.scss";
+
+function HomePage() {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <div className="title">HATS</div>
+            <div className="subtitle">SHOP NOW</div>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <div className="title">JACKETS</div>
+            <div className="subtitle">SHOP NOW</div>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <div className="title">SNEAKERS</div>
+            <div className="subtitle">SHOP NOW</div>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <div className="title">MENS</div>
+            <div className="subtitle">SHOP NOW</div>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <div className="title">WOMENS</div>
+            <div className="subtitle">SHOP NOW</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
